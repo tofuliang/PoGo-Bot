e466d4af78885635f3d858981b63b50f0637ffb3
BoBeR182 <BoBeR182@users.noreply.github.com>
2016-07-29 00:47:51 +0000

    TravisCI greenification

--- pgoapi/poke_utils.py
+++ pgoapi/poke_utils.py
@@ -1,5 +1,6 @@
 import os
 
+
 def pokemon_iv_percentage(pokemon):
     return ((pokemon.get('individual_attack', 0) + pokemon.get('individual_stamina', 0) + pokemon.get(
         'individual_defense', 0) + 0.0) / 45.0) * 100.0
