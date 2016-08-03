57b3b0f91ad0d3e76a335118c1a133387133d2da
morefreeze <morefreeze@gmail.com>
2016-08-03 11:13:08 +0800

    Add get_player_stats in poke_utils

--- pgoapi/poke_utils.py
+++ pgoapi/poke_utils.py
@@ -6,18 +6,20 @@ def pokemon_iv_percentage(pokemon):
         'individual_defense', 0) + 0.0) / 45.0) * 100.0
 
 
-def get_pokemon_num(res):
+def extract_items_dict(res):
     inventory_delta = res['responses']['GET_INVENTORY'].get('inventory_delta', {})
     inventory_items = inventory_delta.get('inventory_items', [])
-    inventory_items_dict_list = map(lambda x: x.get('inventory_item_data', {}), inventory_items)
+    return map(lambda x: x.get('inventory_item_data', {}), inventory_items)
+
+
+def get_pokemon_num(res):
+    inventory_items_dict_list = extract_items_dict(res)
     inventory_items_pokemon_list = filter(lambda x: 'pokemon_data' in x and 'is_egg' not in x['pokemon_data'], inventory_items_dict_list)
     return len(inventory_items_pokemon_list)
 
 
 def get_inventory_data(res, poke_names):
-    inventory_delta = res['responses']['GET_INVENTORY'].get('inventory_delta', {})
-    inventory_items = inventory_delta.get('inventory_items', [])
-    inventory_items_dict_list = map(lambda x: x.get('inventory_item_data', {}), inventory_items)
+    inventory_items_dict_list = extract_items_dict(res)
     inventory_items_pokemon_list = filter(lambda x: 'pokemon_data' in x and 'is_egg' not in x['pokemon_data'],
                                           inventory_items_dict_list)
     inventory_items_pokemon_list_sorted = sorted(inventory_items_pokemon_list, key=lambda x: poke_names[str(x['pokemon_data']['pokemon_id'])].encode('ascii', 'ignore'))
@@ -40,11 +42,17 @@ def get_inventory_data(res, poke_names):
 
 
 def get_incubators_stat(res):
-    inventory_delta = res['responses']['GET_INVENTORY'].get('inventory_delta', {})
-    inventory_items = inventory_delta.get('inventory_items', [])
-    inventory_items_incubators = map(lambda x: x.get('inventory_item_data', {}), inventory_items)
-    inventory_items_dict_list = map(lambda x: x.get('egg_incubators', {}), inventory_items_incubators)
-    inventory_items_incubator_list = filter(lambda x: 'egg_incubator' in x and 'target_km_walked' in x, inventory_items_dict_list)
+    inventory_items_dict_list = extract_items_dict(res)
+    inventory_items_incubators = map(lambda x: x.get('egg_incubators', {}).get('egg_incubator', {}), inventory_items_dict_list)
+    inventory_items_incubator_list = reduce(lambda x, y: x + y, filter(lambda x: len(x) > 0, inventory_items_incubators))
+    player_stats = get_player_stats(res)
+    km_walked = player_stats.get('km_walked', 'None')
+    if inventory_items_incubator_list:
+        return (os.linesep.join(map(lambda x: "Incubator {0:.2f} km, walked {1:.2f} km".format(
+            x['target_km_walked'],
+            km_walked), inventory_items_incubator_list)))
+    else:
+        return 'No incubators'
     if inventory_items_incubator_list:
         return (os.linesep.join(map(lambda x: "Incubator {0:.2f} km, walked {1:.2f} km".format(
             x['egg_incubator']['target_km_walked'],
